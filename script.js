a77871ead6deeb43150ddd100c62d774dbadb31a
Vincent Ma <vincentma.code@gmail.com>
2025-12-06 18:40:27 -0500

    Adding radio buttons with highlight
    Added radio buttons and highlight selected input

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ import { catsData } from '/data.js'
 
 const emotionRadio = document.getElementById('emotion-radios')
 
+//Getting the array of emotions to highlight
+emotionRadio.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight') //removes class highlight
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight') //add highlight
+}
+
 function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
@@ -21,7 +32,17 @@ function renderEmotionsRadios(cats){
     let radioItems = ''
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input 
+                type="radio"
+                id="${emotion}"
+                value="${emotion}"
+                name="emotions"
+                >
+        </div>
+        ` //HTML with the radio buttons
     }
     emotionRadio.innerHTML = radioItems
 }
